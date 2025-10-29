a4f5c78be98b9ff054298a7d42c875b564cbd6e3
Thallys Cezar <thallys.cezar@di2win.com>
2025-10-29 15:35:47 -0300

    :fix: fix and refactor code in the project in the ms-database

--- ms-database/src/test/java/br/com/thallysprojetos/ms_database/DatabaseApplicationTests.java
+++ ms-database/src/test/java/br/com/thallysprojetos/ms_database/DatabaseApplicationTests.java
@@ -1,9 +1,14 @@
 package br.com.thallysprojetos.ms_database;
 
+import br.com.thallysprojetos.ms_database.config.TestRabbitConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = DatabaseApplication.class)
+@ActiveProfiles("test") // Usa application-test.properties com H2 em memória
+@Import(TestRabbitConfig.class) // Importa configuração de teste com RabbitTemplate mockado
 class DatabaseApplicationTests {
 
 	@Test
